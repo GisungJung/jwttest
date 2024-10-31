ce07993f99fba9aa627f355a374ca8b69e9ea41c
kevin <jgs5027@naver.com>
2024-10-31 11:00:18 +0900

    aop log 적용

--- src/main/java/com/gs/jwt/api/controller/JwtController.java
+++ src/main/java/com/gs/jwt/api/controller/JwtController.java
@@ -1,11 +1,13 @@
 package com.gs.jwt.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class JwtController {
-    @GetMapping("/api/main")
+    @GetMapping("/main")
     public String getMain(){
         return "main page";
     }
--- src/main/java/com/gs/jwt/config/SwaggerConf.java
+++ src/main/java/com/gs/jwt/config/SwaggerConf.java
@@ -1,7 +1,11 @@
 package com.gs.jwt.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +16,16 @@ public class SwaggerConf {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .info(new Info()
-                        .title("API Documentation")
+                        .title("jwt test API Document")
                         .version("1.0")
-                        .description("Spring Boot 3 API Documentation with Swagger"));
+                        .contact(new Contact()
+                                .name("kevin")
+                                .email("jgs5027@naver.com"))
+                        .description("test API"));
     }
 
     @Bean
